895c04452448b9ebb3f0f351b4cadde48d4acfc9
nikhilsrivaw <nikhilksrivastav190@gmail.com>
2025-06-22 09:46:03 +0530

    changes

--- backend/src/routes/userAuth.js
+++ backend/src/routes/userAuth.js
@@ -12,3 +12,16 @@ authRouter.post('/login' , login);
 authRouter.post('/logout', userMiddleware , logout);
 authRouter.post('/admin/register', adminMiddleware, adminRegisrter)
 authRouter.delete('/profile' , userMiddleware, deleteProfile)
+authRouter.get('/check',userMiddleware,(req,res)=>{
+    const reply ={
+        firstName:req.result.firstName, 
+
+        emailId:req.result.emailId,
+        _id:req.result._id,
+    }
+
+    res.status(200).json({
+        user:reply,
+        message:"Valid User"
+    })
+})
